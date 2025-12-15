deab3b42f8ec5e35b6ac839d68e3d67dd40b0cd6
Vaibhavee Thorat <93214054+vaibhaveethorat11@users.noreply.github.com>
2025-12-15 21:57:22 +0530

    Document single-line and multi-line comments in Python
    Add examples of single-line and multi-line comments in Python.

---
+++ src/comments.py
@@ -0,0 +1,19 @@
+""" In python there are two types of comments
+1. Single line comments and 2. Multi-line commnents 
+"""
+print "Hello World" #This is a single line comment. It starts from "#"
+
+""" For multi-line comment use three double quotes """
+
+#PUTHON Backlash
+"""
++ It is used for continuation of print statement.
++ Stretch a single statement across multiple lines.
+for eg """
+
+print "HI I \
+am Vaibhavee "
+
+
+
+
