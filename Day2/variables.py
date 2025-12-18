44df6eacd5779bd2cb156eea5a5e9863f3a617c5
Vaibhavee Thorat <93214054+vaibhaveethorat11@users.noreply.github.com>
2025-12-18 23:52:55 +0530

    Add variable naming conventions and examples

---
+++ Day2/variables.py
@@ -0,0 +1,22 @@
+#Legal variable names:
+myvar = "Vaibhavee"
+my_var = "Vaibhavee"
+_my_var = "Vaibhavee"
+myVar = "Vaibhavee"
+MYVAR = "Vaibhavee"
+myvar2 = "Vaibhavee"
+
+#Camel Case
+myName = "Neha"
+
+#Pascal Case
+MyName = "Vaibhavee"
+
+#Snake Case
+my_name = "Vaibhavee"
+
+#Many Values to Multiple Variables
+x, y, z = "Red", "Blue", "Green"
+print(x)
+print(y)
+print(z)
