0f84b06deff8081c50e0e775d5e2300879a52f87
Vaibhavee Thorat <93214054+vaibhaveethorat11@users.noreply.github.com>
2025-12-23 18:07:10 +0530

    Add operator.py with multiple operator examples
    Implement various operators including arithmetic, comparison, logical, assignment, and bitwise in operator.py.

---
+++ Day4/operator.py
@@ -0,0 +1,55 @@
+#Arithmetic operators
+a = 9
+b = 4
+add = a + b 
+sub = a - b 
+mul = a * b 
+mod = a % b 
+exp = a ** b 
+
+print(add) 
+print(sub) 
+print(mul) 
+print(mod) 
+print(exp)
+
+#Comparison Operators
+a = 15
+b = 25
+​
+print(a == b)   
+print(a != b)  
+print(a > b)    
+print(a < b)  
+print(a >= b)  
+print(a <= b)   
+
+#Logical Operators
+a = True
+b = False
+print(a and b) 
+print(a or b) 
+print(not a)
+
+#Assignment Operators
+a = 5
+b = a 
+print(b) 
+b += a 
+print(b) 
+b -= a 
+print(b) 
+b *= a 
+print(b) 
+b <<= a 
+print(b)
+
+#Bitwise Operators
+a = 10
+b = 4
+print(a & b) 
+print(a | b) 
+print(~a) 
+print(a ^ b) 
+print(a >> 2) 
+print(a << 2)
