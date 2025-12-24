e480f42dc0d6d3e6c867e215d98a32f88af7f98c
Vaibhavee Thorat <93214054+vaibhaveethorat11@users.noreply.github.com>
2025-12-24 18:17:24 +0530

    Implement loop examples in loop.py
    Add examples of for loop, while loop, and if-else statement.

---
+++ Day 4/loop.py
@@ -0,0 +1,18 @@
+#for loop
+for i in range(0, 10): 
+    print(i)  # It will print 0 to 9 numbers each on new line
+
+#while loop
+count = 0
+while (count < 3): 
+    count = count + 1      #the condition for while will be True as long as the counter variable (count) is less than 3.
+    print("Hello Learner")  
+
+#if else loop
+i = 10
+if (i < 5): 
+    print("i is smaller than 5") 
+    print("i'm in if Block") 
+else: 
+    print("i is greater than 5")  #The condition i < 5 is False, so the else block will be executed.
+    print("i'm in else Block") 
